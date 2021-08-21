7c8ef06352765dc8ca6c6874e213a74f7ec8cef1
Francisco <public@francisco.io>
2021-08-21 14:32:59 +0200

    Cleaned up unused code

--- rollup.config.js
+++ rollup.config.js
@@ -1,50 +1,18 @@
-import commonjs from "rollup-plugin-commonjs";
-import nodeResolve from "rollup-plugin-node-resolve";
-import json from "rollup-plugin-json";
 import { terser } from "rollup-plugin-terser";
 import babel from "rollup-plugin-babel";
 
 export default {
   input: "src/index.js",
-  output: {
-    file: "index.min.js",
-    name: "crossroad",
-    format: "esm",
-    // This is because we use default and named exports at the same time. The
-    // commonjs will have to import with ['default'], but no problem for ESM
-    exports: "named",
-
-    globals: {
-      react: "React"
-    }
-  },
+  output: { file: "index.min.js", format: "esm" },
   external: ["react"],
   plugins: [
-    nodeResolve({ preferBuiltins: true }),
     babel({
       exclude: "node_modules/**",
       presets: [
-        [
-          "@babel/env",
-          {
-            targets: {
-              node: 12
-            },
-            useBuiltIns: "usage",
-            corejs: 3
-          }
-        ],
+        ["@babel/env", { targets: { node: 12 } }],
         "@babel/preset-react"
       ]
     }),
-
-    commonjs({
-      namedExports: {
-        "react-dom/test-utils": ["act"]
-      }
-    }),
-    json(),
     terser()
-  ],
-  sourceMap: true
+  ]
 };
